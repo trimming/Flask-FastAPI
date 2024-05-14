4cd3c7d4bfe348b4b2f08ee7e14e8ec629dd7432
Alexey Golikov <lexgolikoff@gmail.com>
2024-05-14 23:57:53 +0300

    some update

--- lesson_1/task_8.py
+++ lesson_1/task_8.py
@@ -1,11 +1,9 @@
-# Задание №7
-# Написать функцию, которая будет выводить на экран HTML
-# страницу с блоками новостей.
-# Каждый блок должен содержать заголовок новости,
-# краткое описание и дату публикации.
-# Данные о новостях должны быть переданы в шаблон через
-# контекст
-
+# Задание №8
+# Создать базовый шаблон для всего сайта, содержащий
+# общие элементы дизайна (шапка, меню, подвал), и
+# дочерние шаблоны для каждой отдельной страницы.
+# Например, создать страницу "О нас" и "Контакты",
+# используя базовый шаблон.
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -35,7 +33,39 @@ def news():
             'date': '10.05.2024'
         },
     ]
-    return render_template('news.html', news=news)
+    return render_template('news2.html', news=news)
+
+
+@app.route('/students/')
+def students_info():
+    students = [
+        {
+            'name': 'Alexey',
+            'last_name': 'Golikov',
+            'age': '42',
+            'range': '4.0'
+        },
+        {
+            'name': 'Sergio',
+            'last_name': 'Ramos',
+            'age': '28',
+            'range': '7.2'
+        },
+        {
+            'name': 'James',
+            'last_name': 'Milner',
+            'age': '38',
+            'range': '6.5'
+        },
+    ]
+
+    return render_template('std.html', students=students)
+
+
+
+@app.route('/base/')
+def base():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
